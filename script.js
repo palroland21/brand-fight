1ff2eacd473a39ab09e08ad417342c295bff27b8
palroland21 <78417938+palroland21@users.noreply.github.com>
2022-01-10 22:16:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("vote_for_samsung");
+var y = document.getElementById("vote_for_iphone");
+
+function samsung() {
+alert("You are voted for Samsung");
+} 
+
+
+function iphone() {
+ alert("You are voted for iPhone");
+   } 
